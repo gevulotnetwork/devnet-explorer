4d58c324ebec9db5231afd23b75092104a7fb757
Henri Koski <henri.t.koski@gmail.com>
2024-03-01 09:12:08 +0200

    Add basic go application (#2)

--- cmd/devnet-explorer/main.go
+++ cmd/devnet-explorer/main.go
@@ -1,47 +1,17 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
 	"log/slog"
-	"net/http"
 	"os"
-	"os/signal"
+
+	"github.com/gevulotnetwork/devnet-explorer/app"
 )
 
 func main() {
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, os.Interrupt)
-
-	addr := os.Getenv("SERVER_LISTEN_ADDR")
-	if addr == "" {
-		addr = "127.0.0.1:8383"
-	}
-
-	srv := &http.Server{
-		Addr: addr,
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprint(w, "Hello, World!")
-		}),
-	}
-
-	go func() {
-		<-stop
-		slog.Info("stopping server")
-		err := srv.Shutdown(context.Background())
-		if err != nil && !errors.Is(err, http.ErrServerClosed) {
-			slog.Error("graceful server shutdown failed", slog.Any("error", err))
-			os.Exit(1)
-		}
-	}()
-
-	slog.Info("server starting", slog.String("addr", "http://"+addr))
-	err := srv.ListenAndServe()
-	if err != nil && !errors.Is(err, http.ErrServerClosed) {
-		slog.Error("server run failed", slog.Any("error", err))
+	slog.Info("starting application")
+	if err := app.Run(); err != nil {
+		slog.Error("running application failed", slog.Any("error", err))
 		os.Exit(1)
 	}
-
-	slog.Info("server stopped successfully")
+	slog.Info("application stopped successfully")
 }
