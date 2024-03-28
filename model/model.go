73f198221ad4f44f52ed6b9f70bd2d8068031cc0
Tuomas Mäkinen <1947505+tuommaki@users.noreply.github.com>
2024-03-28 08:16:57 +0200

    Initial implementation for tx & prover search (#19)
    Devnet explorer has a search field where user can give transaction hash
    or prover program hash as an input and the backend should return 50
    newest events for that query.

--- model/model.go
+++ model/model.go
@@ -11,8 +11,8 @@ type Stats struct {
 
 type Event struct {
 	State     string    `json:"state"`
-	TxID      string    `json:"tx_id"`
-	ProverID  string    `json:"prover_id"`
+	TxID      string    `db:"tx_id" json:"tx_id"`
+	ProverID  string    `db:"prover_id" json:"prover_id"`
 	Tag       string    `json:"tag"`
 	Timestamp time.Time `json:"timestamp"`
 }
